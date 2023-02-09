0c5ceb4428027db10eebe18fd21c818072ca8754
emobilis <lawrieabuna@gmail.com>
2023-02-09 09:17:12 +0300

    Data structures and if statements

---
+++ Conditions/if_statement.py
@@ -0,0 +1,19 @@
+dialer = int(input("Enter number: "))
+
+if dialer == 1 :
+    print("Blaze Deals")
+
+elif dialer == 2 :
+    print("Daily Bundles")
+
+elif dialer == 3 :
+    print("Weekly Bundles")
+
+elif dialer == 4 :
+    print("Hourly Bundles")
+
+elif dialer == 5 :
+    print("Okoa data")
+
+else:
+    print("Option not available")
---
+++ Data structures/dictionaries.py
@@ -0,0 +1,4 @@
+friends = {"Louis": 25, "Lisa": 22, "Rita": 26}
+#adding data in dictionaries
+friends["Marie"]=23
+print(friends)
