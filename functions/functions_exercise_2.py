8f0343dca716ae254bd231381ba62666c088cc48
emobilis <lawrieabuna@gmail.com>
2023-02-13 09:10:17 +0300

    function_2

---
+++ functions/functions_exercise_2.py
@@ -0,0 +1,8 @@
+def calculate_area():
+    num1 = int(input("Enter length: "))
+    num2 = int(input("Enter width: "))
+
+    area = num1 * num2
+    print(f"The area is {area}")
+
+calculate_area()
