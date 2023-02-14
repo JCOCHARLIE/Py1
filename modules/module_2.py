5e93612db9fbc0b95fe998e72699e692fc251965
emobilis <lawrieabuna@gmail.com>
2023-02-14 08:49:20 +0300

    Modules intro

---
+++ modules/module_2.py
@@ -0,0 +1,6 @@
+from module_1 import *
+
+print(add_two_numbers(50, 150))
+print(multiply_two_numbers(20, 30))
+print(divide_two_numbers(100, 20))
+print(subtract_two_numbers(80, 30))
--- stringlength.py
+++ stringlength.py
@@ -1,9 +1,5 @@
 
+name = input("Enter your name: ")
 
+print(len(name))
 
-
-num1 = int(input("Enter your first number: "))
-num2 = int(input("Enter your second number: "))
-
-sum = num2+num1
-print(f"The sum of the numbers = {sum}")
